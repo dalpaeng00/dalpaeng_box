414cd3f9a12a6c7d406736c5b669d1eebd216255
dalpaeng00 <65148479+dalpaeng00@users.noreply.github.com>
2020-05-11 15:21:41 +0900

    Update Sort_Selections.java

--- algorithm study/Sort_Selections.java
+++ algorithm study/Sort_Selections.java
@@ -6,7 +6,7 @@ public class Sort_Selection {
 
 	public static void main(String args[]) {
 
-		int [] ary = {23, 96, 35,42,81,19,8,77,50,64};
+		int [] ary = {23, 96, 35, 42, 81, 19, 8, 77, 50, 64};
 		int i,j,tmp,index;
 		
 		for(i=0; i<ary.length-1; i++) {
